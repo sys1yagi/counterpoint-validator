b5da977dfe3f8e79e61fa085ea2f0e7809277fac
Toshihiro Yagi <toshihiro-yagi@cookpad.com>
2016-10-29 17:37:21 +0900

    Add PitchNameConverterTest.

--- base/src/main/java/com/sys1yagi/counterpoint/PitchNameConverter.kt
+++ base/src/main/java/com/sys1yagi/counterpoint/PitchNameConverter.kt
@@ -9,26 +9,53 @@ object PitchNameConverter {
         if (pitchName.isEmpty()) {
             throw InvalidPitchNameException("empty")
         }
-        when (pitchName[0]) {
-            'C' -> {
-
-            }
-            'D' -> {
-
-            }
-            'E' -> {
-
-            }
-            'F' -> {
-
-            }
-            'G' -> {
-
-            }
-            'H' -> {
-
+        return pitchName.toUpperCase().let {
+            when (it[0]) {
+                'C' -> {
+                    return PitchName.C
+                }
+                'D' -> {
+                    if (it[1] == 'F') {
+                        PitchName.DF
+                    } else {
+                        PitchName.D
+                    }
+                }
+                'E' -> {
+                    if (it[1] == 'F') {
+                        PitchName.EF
+                    } else {
+                        PitchName.E
+                    }
+                }
+                'F' -> {
+                    PitchName.F
+                }
+                'G' -> {
+                    if (it[1] == 'F') {
+                        PitchName.GF
+                    } else {
+                        PitchName.G
+                    }
+                }
+                'A' -> {
+                    if (it[1] == 'F') {
+                        PitchName.AF
+                    } else {
+                        PitchName.A
+                    }
+                }
+                'B' -> {
+                    if (it[1] == 'F') {
+                        PitchName.BF
+                    } else {
+                        PitchName.B
+                    }
+                }
+                else -> {
+                    throw InvalidPitchNameException(pitchName)
+                }
             }
         }
-        throw InvalidPitchNameException(pitchName)
     }
 }
--- base/src/test/java/com/sys1yagi/counterpoint/PitchNameConverterTest.kt
+++ base/src/test/java/com/sys1yagi/counterpoint/PitchNameConverterTest.kt
@@ -7,12 +7,52 @@ import org.junit.Test
 class PitchNameConverterTest {
     @Test
     fun stringToPitchNameValid() {
-        
+        run {
+            val pitchName = PitchNameConverter.stringToPitchName("C1")
+            assertThat(pitchName).isEqualTo(PitchName.C)
+        }
+        run {
+            val pitchName = PitchNameConverter.stringToPitchName("D1")
+            assertThat(pitchName).isEqualTo(PitchName.D)
+        }
+        run {
+            val pitchName = PitchNameConverter.stringToPitchName("DF4")
+            assertThat(pitchName).isEqualTo(PitchName.DF)
+        }
+        run {
+            val pitchName = PitchNameConverter.stringToPitchName("F4")
+            assertThat(pitchName).isEqualTo(PitchName.F)
+        }
+        run {
+            val pitchName = PitchNameConverter.stringToPitchName("GF4")
+            assertThat(pitchName).isEqualTo(PitchName.GF)
+        }
+        run {
+            val pitchName = PitchNameConverter.stringToPitchName("G8")
+            assertThat(pitchName).isEqualTo(PitchName.G)
+        }
+        run {
+            val pitchName = PitchNameConverter.stringToPitchName("AF3")
+            assertThat(pitchName).isEqualTo(PitchName.AF)
+        }
+        run {
+            val pitchName = PitchNameConverter.stringToPitchName("A4")
+            assertThat(pitchName).isEqualTo(PitchName.A)
+        }
+        run {
+            val pitchName = PitchNameConverter.stringToPitchName("BF4")
+            assertThat(pitchName).isEqualTo(PitchName.BF)
+        }
+        run {
+            val pitchName = PitchNameConverter.stringToPitchName("B2")
+            assertThat(pitchName).isEqualTo(PitchName.B)
+        }
     }
 
     @Test
     fun lowerCase() {
-        PitchNameConverter.stringToPitchName("c1")
+        val pitchName = PitchNameConverter.stringToPitchName("c1")
+        assertThat(pitchName).isEqualTo(PitchName.C)
     }
 
     @Test(expected = InvalidPitchNameException::class)
