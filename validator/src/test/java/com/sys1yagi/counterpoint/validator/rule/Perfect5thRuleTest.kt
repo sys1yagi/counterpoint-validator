e36838e97934094bc356e89fbe7f9123b756d809
sys1yagi <sylc.yagi@gmail.com>
2016-11-06 19:56:48 +0900

    Implement Perfect5thRule.

--- validator/src/test/java/com/sys1yagi/counterpoint/validator/rule/Perfect5thRuleTest.kt
+++ validator/src/test/java/com/sys1yagi/counterpoint/validator/rule/Perfect5thRuleTest.kt
@@ -2,18 +2,16 @@ package com.sys1yagi.counterpoint.validator.rule
 
 import com.sys1yagi.counterpoint.Interval
 import com.sys1yagi.counterpoint.validator.exception.WholeNoteCounterPointInvalidException
-import org.junit.Assert.*
-import org.junit.Test
-import org.assertj.core.api.Assertions.assertThat
 import org.junit.Before
+import org.junit.Test
 
-class Perfect5RuleTest {
+class Perfect5thRuleTest {
 
-    lateinit var rule: Perfect5Rule
+    lateinit var rule: Perfect5thRule
 
     @Before
     fun setUp() {
-        rule = Perfect5Rule()
+        rule = Perfect5thRule()
     }
 
     @Test
@@ -39,7 +37,14 @@ class Perfect5RuleTest {
         rule.validation(intervals)
     }
 
-    //オクターブ
+    @Test(expected = WholeNoteCounterPointInvalidException::class)
+    fun octave(){
+        val intervals = listOf(
+                Interval.create("C4", "G5"),
+                Interval.create("D4", "A6")
+        )
+        rule.validation(intervals)
+    }
 
     @Test(expected = WholeNoteCounterPointInvalidException::class)
     fun invalid(){
@@ -49,8 +54,8 @@ class Perfect5RuleTest {
                 Interval.create("E4","G5"),
                 Interval.create("A4","F5"),
                 Interval.create("G4","G5"),
-                Interval.create("D4","A4"),
-                Interval.create("F4","C5"),
+                Interval.create("D4","A4"),  // perfect 5th
+                Interval.create("F4","C5"),  // perfect 5th
                 Interval.create("E4","C5"),
                 Interval.create("D4","B4"),
                 Interval.create("C4","C5")
@@ -58,5 +63,20 @@ class Perfect5RuleTest {
         rule.validation(intervals)
     }
 
-    // 1個はさむ
+    @Test(expected = WholeNoteCounterPointInvalidException::class)
+    fun invalid2(){
+        val intervals = listOf(
+                Interval.create("C4","C5"),
+                Interval.create("D4","B4"),
+                Interval.create("E4","G5"),
+                Interval.create("A4","F5"),
+                Interval.create("G4","G5"),
+                Interval.create("D4","A4"),  // perfect 5th
+                Interval.create("E4","C5"),
+                Interval.create("F4","C5"),  // perfect 5th
+                Interval.create("D4","B4"),
+                Interval.create("C4","C5")
+        )
+        rule.validation(intervals)
+    }
 }
