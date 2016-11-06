b8b2219e74c0582b19d00254cbfdd19b38ca0955
sys1yagi <sylc.yagi@gmail.com>
2016-11-06 20:23:19 +0900

    Implement Perfect8thRule.

---
+++ validator/src/main/java/com/sys1yagi/counterpoint/validator/rule/Perfect8thRule.kt
@@ -0,0 +1,26 @@
+package com.sys1yagi.counterpoint.validator.rule
+
+import com.sys1yagi.counterpoint.Interval
+import com.sys1yagi.counterpoint.validator.exception.WholeNoteCounterPointInvalidException
+
+class Perfect8thRule : WholeNoteCounterPointRule {
+
+    override fun validation(intervals: List<Interval>) {
+        val origin = intervals.map { it.intervalInt }
+        val diff1 = intervals.drop(1).map { it.intervalInt }
+        val diff2 = intervals.drop(2).map { it.intervalInt }
+
+        origin.zip(diff1).forEachIndexed { i, pair ->
+            check(i, pair.first, pair.second)
+        }
+        origin.zip(diff2).forEachIndexed { i, pair ->
+            check(i, pair.first, pair.second)
+        }
+    }
+
+    private fun check(index: Int, first: Int, second: Int) {
+        if (first == Interval.INTERVAL_PERFECT_8 && second == Interval.INTERVAL_PERFECT_8) {
+            throw WholeNoteCounterPointInvalidException("Consecutive perfect 8th. pos=$index")
+        }
+    }
+}
---
+++ validator/src/test/java/com/sys1yagi/counterpoint/validator/rule/Perfect8thRuleTest.kt
@@ -0,0 +1,86 @@
+package com.sys1yagi.counterpoint.validator.rule
+
+import com.sys1yagi.counterpoint.Interval
+import com.sys1yagi.counterpoint.validator.exception.WholeNoteCounterPointInvalidException
+import org.junit.Before
+import org.junit.Test
+
+class Perfect8thRuleTest {
+
+    lateinit var rule: Perfect8thRule
+
+    @Before
+    fun setUp() {
+        rule = Perfect8thRule()
+    }
+
+    @Test
+    fun valid() {
+        run {
+            val intervals = listOf(
+                    Interval.create("C4", "G4"),
+                    Interval.create("D4", "E4")
+            )
+            rule.validation(intervals)
+        }
+        run {
+            val intervals = listOf(
+                    Interval.create("C4", "C6"),
+                    Interval.create("C4", "C6")
+            )
+            rule.validation(intervals)
+        }
+    }
+
+    @Test(expected = WholeNoteCounterPointInvalidException::class)
+    fun samePitch() {
+        val intervals = listOf(
+                Interval.create("C4", "C5"),
+                Interval.create("C4", "C5")
+        )
+        rule.validation(intervals)
+    }
+
+    @Test(expected = WholeNoteCounterPointInvalidException::class)
+    fun consecutivePerfect5() {
+        val intervals = listOf(
+                Interval.create("C4", "C5"),
+                Interval.create("D4", "D5")
+        )
+        rule.validation(intervals)
+    }
+
+    @Test(expected = WholeNoteCounterPointInvalidException::class)
+    fun invalid() {
+        val intervals = listOf(
+                Interval.create("C4", "C5"),
+                Interval.create("D4", "B4"),
+                Interval.create("E4", "G5"),
+                Interval.create("A4", "F5"),
+                Interval.create("G4", "G5"),
+                Interval.create("D4", "D5"), // perfect 8th
+                Interval.create("C5", "C5"), // perfect 8th
+                Interval.create("E4", "C5"),
+                Interval.create("D4", "B4"),
+                Interval.create("C4", "C5")
+        )
+        rule.validation(intervals)
+    }
+
+    @Test(expected = WholeNoteCounterPointInvalidException::class)
+    fun invalid2() {
+        val intervals = listOf(
+                Interval.create("C4", "C5"),
+                Interval.create("D4", "B4"),
+                Interval.create("E4", "G5"),
+                Interval.create("A4", "F5"),
+                Interval.create("G4", "G5"),
+                Interval.create("D4", "D5"), // perfect 8th
+                Interval.create("E4", "C5"),
+                Interval.create("F4", "F5"), // perfect 8th
+                Interval.create("D4", "B4"),
+                Interval.create("C4", "C5")
+        )
+        rule.validation(intervals)
+    }
+}
